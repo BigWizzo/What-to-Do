c002ce17371ebbb7c0f956f63c303f0cb3e23227
bigwizzo <wnnwillard@gmail.com>
2021-02-24 19:18:46 +0200

    Add Project class

---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+export default class Project {
+  constructor(proTitle, todoTitle, description, dueDate, priority, notes, checkList) {
+    this.proTitle = proTitle
+    this.todos = {}
+    this.todos.todo = {}
+    this.todos.todo.todoTitle = todoTitle
+    this.todos.todo.desc = description
+    this.todos.todo.dueDate = dueDate
+    this.todos.todo.priority = priority
+    this.todos.todo.notes = notes
+    this.todos.todo.checkList = checkList
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./scss/styles.scss"
+import Project from "./components/projects";
+
+const project12 = new Project('proTitle12')
+console.log(project12)
