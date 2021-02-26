1b3c75c4b1d8e0efb7b30a4136c411552b63f8b1
Khristian Rojas <khristianrojas15@gmail.com>
2021-02-26 16:58:28 -0300

    Add styles to todos

--- src/components/UI.js
+++ src/components/UI.js
@@ -39,14 +39,27 @@ export default class UI {
         prjc.todos.forEach(function(todo) {
             // const todosWrap = document.querySelector(`#v-pills-${UI.urlSlug(prjc.proTitle)}`);
             const todoItem = document.createElement('div');
+            todoItem.setAttribute("class", "row");
             console.log(todo.todoTitle);
-            todoItem.innerHTML = `<li class="long">${todo.todoTitle}</li>
-            <li class="long">${todo.description}</li>
-            <li class="short">${todo.dueDate}</li>
-            <li class="short">${todo.priority}</li>
-            <li class="long">${todo.notes}</li>
-            <li class="short">${todo.checkList}</li>
-            <li class="short"><a href="#" class="btn btn-danger btn-sm delete">X</a></li>`;
+            todoItem.innerHTML = `
+            <div class="col-10 row">
+            <div class="col-12 col-md-6 row">
+            <li class="col">${todo.todoTitle}</li>
+            <li class="col">${todo.description}</li>
+            <li class="col">${todo.dueDate}</li>
+            </div>
+            <div class="col-12 col-md-6 row">
+            <li class="col">${todo.priority}</li>
+            <li class="col">${todo.notes}</li>
+            <li class="col">${todo.checkList}</li>
+            </div>
+            </div>
+            <div class="col-2 row">
+            <span class="col-6"><a href="#" class="btn btn-danger btn-sm delete"><i class="far fa-trash-alt"></i></a></span>
+            <span class="col-6"><a href="#" class="btn btn-primary btn-sm delete"><i class="far fa-edit"></i></a></span>
+            </div>
+            `
+            ;
             target.appendChild(todoItem);
         });
         content.appendChild(target);
@@ -55,7 +68,7 @@ export default class UI {
     static addButton() {
       const content = document.querySelector('#v-pills-tabContent');
       const addButton = document.createElement('div');
-        addButton.setAttribute("class","btn btn-light addbtn w-100 bolder text-primary");
+        addButton.setAttribute("class","btn btn-light addbtn w-100 bolder text-primary shadow mt-3");
         addButton.innerText = "+";
         addButton.addEventListener('click', e => {
             showModal();
