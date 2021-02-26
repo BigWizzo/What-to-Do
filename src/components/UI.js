2352452005ab1ffa336208e207805399f7a49b5a
Khristian Rojas <khristianrojas15@gmail.com>
2021-02-26 11:39:52 -0300

    Add todos forEach

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,35 +1,22 @@
 import Store from './storage.js';
-
-
-const projects = Store.getProjects();
 //UI class
 export default class UI {
-    static urlSlug(title) {
-          return title
-            .split(/\W/)
-            .filter(obj => {
-              return obj !== "";
-            })
-            .join("-")
-            .toLowerCase();
+    static urlSlug(title) {
+          return title
+            .split(/\W/)
+            .filter(obj => {
+              return obj !== "";
+            })
+            .join("-")
+            .toLowerCase();
     }
-
     static displayProjects() {
         const projects = Store.getProjects();
-        
-        projects.forEach((project, index) => {
-            UI.addProjects(project,index)
-        });
-        showProjects()
+        projects.forEach((project) => UI.addProjects(project));
     }
-
-    
-    static addProjects(prjc, index) {
+    static addProjects(prjc) {
         const menu = document.querySelector('#v-pills-tab');
-        const button = document.createElement('a');
-        const content = document.querySelector('#v-pills-tabContent');
-        const target = document.createElement('div');
-        
+        const button = document.createElement('a');        
         button.setAttribute("class", "nav-link");
         button.setAttribute("id", `v-pills-${UI.urlSlug(prjc.proTitle)}-tab`);
         button.setAttribute("data-toggle", "pill");
@@ -38,40 +25,27 @@ export default class UI {
         button.setAttribute("aria-controls", `v-pills-${UI.urlSlug(prjc.proTitle)}`);
         button.setAttribute("aria-selected", "false");
         button.innerText = `${prjc.proTitle}`;
-        
+        menu.appendChild(button);
+        console.log(prjc.todos);
+        const content = document.querySelector('#v-pills-tabContent');
+        const target = document.createElement('div');
         target.setAttribute("class", "tab-pane fade");
         target.setAttribute("id", `v-pills-${UI.urlSlug(prjc.proTitle)}`);
         target.setAttribute("aria-labelledby", `v-pills-${UI.urlSlug(prjc.proTitle)}`);
         target.setAttribute("role", "tabpanel");
-        target.innerHTML = `${projects[index].todos.map((todo) => {
-            showToDos(todo)
-            console.log(showToDos(index));
-        })}`;
-        menu.appendChild(button);
+        prjc.todos.forEach(function(todo) {
+            // const todosWrap = document.querySelector(`#v-pills-${UI.urlSlug(prjc.proTitle)}`);
+            const todoItem = document.createElement('div');
+            console.log(todo.todoTitle);
+            todoItem.innerText = `${todo.todoTitle}`;
+            target.appendChild(todoItem);
+        });
         content.appendChild(target);
     }
-};
-
- const showToDos = (todo) => {
-    const row = document.createElement('ul')
-    
-    row.innerHTML = `
-    <li>${todo.todoTitle}</li>
-    <li>${todo.description}</li>
-    <li>${todo.dueDate}</li>
-    <li>${todo.priority}</li>
-    <li>${todo.notes}</li>
-    <li>${todo.checkList}</li>
-    <li><a href="#" class="btn btn-danger btn-sm delete">X</a></li>`;
-    return row;
-}
-
-const showProjects = () => {
-    const list = document.querySelectorAll('.nav-link');
-    
-    list.forEach(project => {
-        project.addEventListener('click', e => {
-            console.log(e.target.id);
-        })
-    })
+    static clearFields() {
+    }
+    static deleteBook(el) {
+      }
+   static showAlert(message, className) {
+   }
 }
