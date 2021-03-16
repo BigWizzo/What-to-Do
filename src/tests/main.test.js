a78f2e98ef60f2faa0358402318e8dd2932b76a5
Khristian Rojas <khristianrojas15@gmail.com>
2021-03-16 19:16:53 -0300

    Add unit test for main

--- src/tests/main.test.js
+++ src/tests/main.test.js
@@ -1,9 +1,5 @@
-// import Tabs from '../components/main';
+import Tabs from '../components/main';
 
-// test('initial display of tabs', () => {
-//   expect(Tabs.to_s).toMatch(/content/);
-// });
-
-test('adds 1 + 2 to equal 3', () => {
-  expect((1 + 2)).toBe(3);
+test('initial display of tabs', () => {
+  expect(Tabs.createTabs.to_s).toMatch(/content/);
 });
