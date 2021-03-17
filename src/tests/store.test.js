f98239a60ce80fb4100a2a64bbb18b618c873dd9
bigwizzo <wnnwillard@gmail.com>
2021-03-17 19:17:21 +0200

    Add tests for Store and update the README file

---
+++ src/tests/store.test.js
@@ -0,0 +1,27 @@
+import Store from '../components/storage';
+
+describe('Store', () => {
+  it('Store is a function', () => {
+    expect(typeof Store).toBe('function');
+  });
+
+  it('getProjects is a function', () => {
+    expect(typeof Store.getProjects).toBe('function');
+  });
+
+  it('addToDo is a function', () => {
+    expect(typeof Store.addToDo).toBe('function');
+  });
+
+  it('removeToDo is a function', () => {
+    expect(typeof Store.removeToDo).toBe('function');
+  });
+
+  it('updateToDo is a function', () => {
+    expect(typeof Store.updateToDo).toBe('function');
+  });
+
+  it('removePr is a function', () => {
+    expect(typeof Store.removePr).toBe('function');
+  });
+})
